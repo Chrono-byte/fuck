7cd7a15e0986581c556ade5e970d51802a2344ee
Grey <chronomly@gmail.com>
2019-10-09 10:40:45 -0400

    Fix flaw in install script
    * feat: begin install script
    
    * add decorative install script
    
    * fix: Remove flaw in install script
    
    Git could open your text editor to resolve merge conflicts

--- install.sh
+++ install.sh
@@ -5,10 +5,11 @@ echo "Prepare for install!"
 
 # I know that if someone is running this script they probably just cloned the repo and are up to date but still
 echo "Making sure we are up to date"
-git pull origin master -q
+git fetch --all
+git reset --hard origin/master -q
 echo "All done updating"
 
-# Amazing fake loading bar! totally not from stack overflow/exchange
+# Amazing fake loading bar! Totally not from stack overflow/exchange
 sudo cp -i ./fuck.sh /usr/bin/fuck
 echo -ne '#####                     (33%)\r'
 sleep 1
